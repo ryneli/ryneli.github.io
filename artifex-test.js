e5ffd9429d93aa37420081d8d13c4e7cb28d5882
Zhenqiang Li <lizhenqiangsnake@gmail.cm>
2019-04-09 16:38:58 -0400

    update

--- artifex-test.js
+++ artifex-test.js
@@ -1,1 +1,20 @@
 console.log("artifex test");
+
+class Wow {
+    constructor(svgEl, startStrokeFn, updateStrokeFn, endStrokeFn) {
+        this.svgEl = svgEl;
+        this.startStrokeFn = startStrokeFn;
+        this.updateStrokeFn = updateStrokeFn;
+        this.endStrokeFn = endStrokeFn;
+    }
+
+    onTouchStart(e) {
+        console.log('artifex: %o', e);
+    }
+    onTouchMove(e) {
+        console.log('artifex: %o', e);
+    }
+    onTouchEnd(e) {
+        console.log('artifex: %o', e);
+    }
+}
