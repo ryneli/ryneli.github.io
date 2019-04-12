ba62c9ed13a1043b829156acc7aeb4c643b52f4a
Zhenqiang Li <lizhenqiangsnake@gmail.cm>
2019-04-12 12:55:47 -0400

    add a notepad demo

--- notepad.js
+++ notepad.js
@@ -3,7 +3,7 @@
   
     template.innerHTML = `
         <style>
-          .notepad {
+          #notepad {
             font-size: 2.5rem;
             color: hotpink;
             font-family: monospace;
@@ -17,16 +17,89 @@
             background-color: green;
           }
         </style>
-        <svg class='notepad'>svg is not support here</svg>
+        <svg id='notepad'>svg is not support here</svg>
     `;
   
     class NotePad extends HTMLElement {
-      constructor() {
-        super();
-  
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-      }
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+            this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+            this.svg = this.shadowRoot.getElementById('notepad');
+            this.initPath();
+            this.initListeners();
+        }
+
+        
+
+        initPath() {
+            this.path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            this.path.setAttribute('stroke', 'black');
+            this.path.setAttribute('fill', 'none');
+            this.path.setAttribute('stroke-width', '3');
+            this.d = '';
+        }
+
+        initListeners() {
+            this.svg.addEventListener("touchstart", (e) => {
+                const rect = e.srcElement.getBoundingClientRect();
+                const touch = e.touches[0];
+                this.startStroke(touch.clientX - rect.left, touch.clientY - rect.top);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("touchmove", (e) => {
+                const rect = e.srcElement.getBoundingClientRect();
+                this.updateStroke(touch.clientX - rect.left, touch.clientY - rect.top);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("touchend", (e) => {
+                const rect = e.srcElement.getBoundingClientRect();
+                this.endStroke(touch.clientX - rect.left, touch.clientY - rect.top);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("mousedown", (e) => {
+                this.startStroke(e.offsetX, e.offsetY);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("mousemove", (e) => {
+                this.updateStroke(e.offsetX, e.offsetY);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("mouseup", (e) => {
+                this.endStroke(e.offsetX, e.offsetY);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("pointerdown", (e) => {
+                this.startStroke(e.offsetX, e.offsetY);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("pointermove", (e) => {
+                this.updateStroke(e.offsetX, e.offsetY);
+                e.preventDefault();
+            });
+            this.svg.addEventListener("pointerup", (e) => {
+                this.endStroke(e.offsetX, e.offsetY);
+                e.preventDefault();
+            });
+        }
+
+        startStroke(x, y) {
+            this.initPath()
+            this.d = `M ${x} ${y}`;
+            this.svg.append(this.path);
+        }
+
+        updateStroke(x, y) {
+            if (this.d.startsWith('M')) {
+                this.d = `${this.d} L ${x} ${y}`;
+                this.path.setAttribute('d', `${this.d}`);
+            }
+        }
+
+        endStroke(x, y) {
+            this.initPath();
+        }
     }
   
     window.customElements.define('note-pad', NotePad);
